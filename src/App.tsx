50b0b2f4a130e301a35a61a61cf8b51585095363
muktanshumishra24 <77049812+muktanshumishra24@users.noreply.github.com>
2021-06-23 13:12:37 +0530

    [#17] Implemented basic canvas (#27)
    Co-authored-by: humble_D <aakashs.mallik.cse15@itbhu.ac.in>

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from 'pages/dashboard';
 import Settings from 'pages/settings';
+import Canvas from 'pages/canvas';
 import Auth from 'pages/auth';
 import AuthProvider from 'provider/auth';
 import AppShell from './components/app-shell';
 
 function App(): JSX.Element {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route element={<Auth />} path="/login" />
-          <Route element={<AppShell />} path="/">
-            <Route element={<Dashboard />} path="/dashboard" />
-            <Route element={<Settings />} path="/settings" />
-          </Route>
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route element={<Auth />} path="/login" />
+        <Route element={<AppShell />} path="/">
+          <Route element={<Canvas />} path="/canvas" />
+          <Route element={<Dashboard />} path="/dashboard" />
+          <Route element={<Settings />} path="/settings" />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/canvas/canvas.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Canvas(): JSX.Element {
+  return <div>Canvas here!!</div>;
+}
+
+export default Canvas;
